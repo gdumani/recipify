3d4d7e6939985cf0d85efe5db541e27fff43aa6a
jic <ji_camargo@yahoo.com>
2023-09-12 13:40:11 -0500

    create devise user views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :foods, dependent: :destroy
-  has many :recipipes, dependent: :destroy
+  has_many :recipipes, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50, minimum: 1 }
 end
