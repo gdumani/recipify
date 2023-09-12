a8a772795d182d07241a612f1da10b4c5a52ab13
gdumani <gdumani@gmail.com>
2023-09-12 12:05:48 -0600

    Fix gitignore to include assests for bootstrap Add models validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :foods, dependent: :destroy
+  has many :recipipes, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50, minimum: 1 }
 end
