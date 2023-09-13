fcf7280bda2da7077ad245df2b0b483ee5eab77d
jic <ji_camargo@yahoo.com>
2023-09-13 17:16:58 -0500

    add shopping list controller and view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
-  has_and_belongs_to_many :foods, through: :recipe_foods
+  has_many :foods, through: :recipe_foods
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
